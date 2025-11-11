3f3312b22f55c1591ccee6f9a8386d05f74728b9
Niket Jain <niketjain@Nikets-MacBook-Air-4.local>
2025-11-11 11:33:25 -0500

    adding rmsnorm

---
+++ rmsnorm.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+class RMSNorm(nn.Module):
+    def __init__(self, d_model, eps=1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weights = nn.Parameter(torch.ones(d_model))
+
+    def forward(self, x):
+        print("how x looks",x)
+        rms = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + self.eps)
+        print("this is rms", rms)
+        print(self.weights)
+        output = (x / rms) * self.weights
+        return output
+
+#testing
+# x = torch.tensor([
+#     [2, -1, 3, -2],
+#     [1, 2, -1, 1.5],
+#     [-1, 0.5, 2, -0.5]
+# ])
+
+# norm = RMSNorm(4)
+# output = norm(x)
+# print(output.shape)
